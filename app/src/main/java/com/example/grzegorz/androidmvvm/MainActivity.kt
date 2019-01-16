0df3062acbd360cc00d3ecfa21ffb5ed9db68808
Grzesiek <grzegorz.kwasniewski@itcraft.pl>
2019-01-16 21:23:14 +0100

    ADD Hook Download Button With KTX And RxBinding

--- app/src/main/java/com/example/grzegorz/androidmvvm/MainActivity.kt
+++ app/src/main/java/com/example/grzegorz/androidmvvm/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.grzegorz.androidmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jakewharton.rxbinding2.view.clicks
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        disposable = downloadButton.clicks()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                println("click")
+            }
+    }
+
+    override fun onPause() {
+        disposable?.dispose()
+        super.onPause()
     }
 }
