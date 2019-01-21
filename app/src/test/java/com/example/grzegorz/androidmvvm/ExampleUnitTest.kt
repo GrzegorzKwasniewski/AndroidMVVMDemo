2839acc6fb995deca45b9ffd00004b8e0f087252
Grzesiek <grzegorz.kwasniewski@itcraft.pl>
2019-01-21 20:46:46 +0100

    ADD Api Example Test

--- app/src/test/java/com/example/grzegorz/androidmvvm/ExampleUnitTest.kt
+++ app/src/test/java/com/example/grzegorz/androidmvvm/ExampleUnitTest.kt
@@ -7,45 +7,41 @@ import org.junit.Assert.*
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.internal.schedulers.ExecutorScheduler
-import androidx.annotation.NonNull
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.grzegorz.androidmvvm.mainView.network.ApiService
 import io.reactivex.disposables.Disposable
 import io.reactivex.Scheduler
-import org.junit.rules.TestRule
-import org.junit.runner.Description
-import org.junit.runners.model.Statement
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
-import org.junit.ClassRule
 import org.junit.BeforeClass
 import org.junit.Rule
 
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
     private val viewModel = MainViewModel(ApiServiceMock())
+    private val apiService = ApiService()
+
 
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-
-    @Test
-    fun testToTest() {
-
+    fun viewModelTest() {
         viewModel.getCoinsData()
 
         assertEquals(viewModel.coinsCount, 2)
-
+        assertTrue(viewModel.coinsCount > 0)
     }
+
+    @Test
+    fun apiTest() {
+        apiService.getAllCoins()
+            .subscribe {
+                assertTrue(it.count() > 0)
+            }
+    }
+
 
     companion object {
         @BeforeClass @JvmStatic fun setUpRxSchedulers() {
