ac8d49d12f37fbd1b88dc621a8a5b2fa6c10e124
Grzesiek <grzegorz.kwasniewski@itcraft.pl>
2019-01-21 19:45:14 +0100

    FIX Issue With LiveData Test

--- app/src/test/java/com/example/grzegorz/androidmvvm/ExampleUnitTest.kt
+++ app/src/test/java/com/example/grzegorz/androidmvvm/ExampleUnitTest.kt
@@ -1,8 +1,25 @@
 package com.example.grzegorz.androidmvvm
 
+import com.example.grzegorz.androidmvvm.mainView.viewModel.MainViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import androidx.annotation.NonNull
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.disposables.Disposable
+import io.reactivex.Scheduler
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+import org.junit.ClassRule
+import org.junit.BeforeClass
+import org.junit.Rule
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +27,44 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val viewModel = MainViewModel(ApiServiceMock())
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testToTest() {
+
+        viewModel.getCoinsData()
+
+        assertEquals(viewModel.coinsCount, 2)
+
+    }
+
+    companion object {
+        @BeforeClass @JvmStatic fun setUpRxSchedulers() {
+            val immediate = object : Scheduler() {
+                override fun scheduleDirect(run: Runnable, delay: Long, unit: TimeUnit): Disposable {
+                    // this prevents StackOverflowErrors when scheduling with a delay
+                    return super.scheduleDirect(run, 0, unit)
+                }
+
+                override fun createWorker(): Scheduler.Worker {
+                    return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
+                }
+            }
+
+            RxJavaPlugins.setInitIoSchedulerHandler { scheduler -> immediate }
+            RxJavaPlugins.setInitComputationSchedulerHandler { scheduler -> immediate }
+            RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler -> immediate }
+            RxJavaPlugins.setInitSingleSchedulerHandler { scheduler -> immediate }
+            RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> immediate }
+        }
     }
 }
